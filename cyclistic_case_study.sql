eae9ec03b673cf0489b2f3ddd40dea159fa28e47
Dmasi1992 <105881038+Dmasi1992@users.noreply.github.com>
2022-05-22 19:25:28 -0400

    Update cyclistic_case_study.sql

--- cyclistic_case_study.sql
+++ cyclistic_case_study.sql
@@ -1,142 +1,165 @@
 WITH
 
-    combined_raw_data AS 
+    combined_raw_data AS
+
 
     (
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
-        `cyclistic-344922.cyclistic_td.202103_td` 
-    UNION ALL 
-
+        EXCEPT (start_station_id, end_station_id)
+    FROM
+        `cyclistic-344922.cyclistic_td.202103_td`
+    UNION ALL
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202104_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202105_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202106_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202107_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202108_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202109_td`
     UNION ALL
-        
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202110_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202111_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202112_td`
-    UNION ALL
-
+    UNION ALL 
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202201_td`
     UNION ALL
-
     SELECT*
-        EXCEPT(start_station_id, end_station_id)
-    FROM 
+        EXCEPT (start_station_id, end_station_id)
+    FROM
         `cyclistic-344922.cyclistic_td.202202_td`
     ),
 
 
     remove_nulls AS 
-
     (
     SELECT*
     FROM
         combined_raw_data
     WHERE
-        ride_id IS NOT NULL
-            AND rideable_type IS NOT NULL
-                AND started_at IS NOT NULL
-                    AND ended_at IS NOT NULL
-                        AND start_station_name IS NOT NULL
-                            AND end_station_name IS NOT NULL
-                                AND start_lat IS NOT NULL
-                                    AND start_lng IS NOT NULL
-                                        AND end_lat IS NOT NULL
-                                            AND end_lng IS NOT NULL
-                                                AND member_casual IS NOT NULL    
+        ride_id IS NOT NULL AND
+        rideable_type IS NOT NULL AND
+        started_at IS NOT NULL AND
+        ended_at IS NOT NULL AND
+        start_station_name IS NOT NULL AND
+        end_station_name IS NOT NULL AND
+        start_lat IS NOT NULL AND
+        start_lng IS NOT NULL AND
+        end_lat IS NOT NULL AND
+        end_lng IS NOT NULL AND
+        member_casual IS NOT NULL    
     ),
 
-    agg_data AS 
-    
+
+    ride_day_of_week AS 
     (
-    SELECT*,
-    TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS ride_length_minutes,
-
-    CASE
-      WHEN EXTRACT(DAYOFWEEK FROM started_at) = 1 THEN 'Sunday'
-      WHEN EXTRACT(DAYOFWEEK FROM started_at) = 2 THEN 'Monday'
-      WHEN EXTRACT(DAYOFWEEK FROM started_at) = 3 THEN 'Tuesday'
-      WHEN EXTRACT(DAYOFWEEK FROM started_at) = 4 THEN 'Wednesday'
-      WHEN EXTRACT(DAYOFWEEK FROM started_at) = 5 THEN 'Thursday'
-      WHEN EXTRACT(DAYOFWEEK FROM started_at) = 6 THEN 'Friday'
-      ELSE 'Saturday'
-    
-    END AS day_of_week
+    SELECT *,
+        TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS ride_length_minute,
+        CASE
+        WHEN EXTRACT(DAYOFWEEK FROM started_at) = 1 THEN 'Sunday'
+        WHEN EXTRACT(DAYOFWEEK FROM started_at) = 2 THEN 'Monday'
+        WHEN EXTRACT(DAYOFWEEK FROM started_at) = 3 THEN 'Tuesday'
+        WHEN EXTRACT(DAYOFWEEK FROM started_at) = 4 THEN 'Wednesday'
+        WHEN EXTRACT(DAYOFWEEK FROM started_at) = 5 THEN 'Thursday'
+        WHEN EXTRACT(DAYOFWEEK FROM started_at) = 6 THEN 'Friday'
+        ELSE 'Saturday'
+        END
+        AS day_of_week
     FROM
-        remove_nulls),
+        remove_nulls
+    ),
 
 
+
+    ride_month AS 
+    (
+    SELECT *,
+ 
+        CASE
+        WHEN EXTRACT(MONTH FROM started_at) = 1 THEN 'January'
+        WHEN EXTRACT(MONTH FROM started_at) = 2 THEN 'February'
+        WHEN EXTRACT(MONTH FROM started_at) = 3 THEN 'March'
+        WHEN EXTRACT(MONTH FROM started_at) = 4 THEN 'April'
+        WHEN EXTRACT(MONTH FROM started_at) = 5 THEN 'May'
+        WHEN EXTRACT(MONTH FROM started_at) = 6 THEN 'June'
+        WHEN EXTRACT(MONTH FROM started_at) = 7 THEN 'July'
+        WHEN EXTRACT(MONTH FROM started_at) = 8 THEN 'August'
+        WHEN EXTRACT(MONTH FROM started_at) = 9 THEN 'September'
+        WHEN EXTRACT(MONTH FROM started_at) = 10 THEN 'October'
+        WHEN EXTRACT(MONTH FROM started_at) = 11 THEN 'November'
+        ELSE 'December'
+        END
+        AS ride_month
+    FROM
+        ride_day_of_week
+    ),
+
+    ride_start_hour AS 
+    (
+    SELECT *,
+ 
+    EXTRACT (HOUR FROM started_at) AS start_hour
+    
+    FROM
+        ride_month
+    ),
+    
+    
     cleaned_data AS 
-
     (
     SELECT*
     FROM 
-        agg_data 
+        ride_start_hour  
     WHERE
-        LENGTH(ride_id) = 16
-            AND ride_length_minutes >= 1
+        LENGTH(ride_id) = 16 AND
+        ride_length_minute >= 1
     ),
 
 
     clean_coord AS 
-
     (
     SELECT
         ride_id,
         ROUND(start_lat,5) AS start_lat,
         ROUND(end_lat,5) AS end_lat,
         ROUND(start_lng,5) AS start_long,
-        ROUND(end_lng,5) AS end_long
+        ROUND(end_lng,5) AS end_long,
+        
     FROM
         cleaned_data
     ),
@@ -156,11 +179,13 @@ WITH
     join_td AS 
     (
     SELECT
-        cd.ride_id,
-        cd.rideable_type,
-        cd.member_casual,
-        cd.ride_length_minutes,
+        cd.ride_id AS trip_id,
+        cd.rideable_type AS bike_type,
+        cd.member_casual AS user_type,
+        cd.ride_length_minute AS length_of_ride,
+        cd.ride_month AS month,
         cd.day_of_week,
+        cd.start_hour AS ride_time,
         cd.start_station_name,
         ll.starting_lat_long,
         cd.end_station_name,
@@ -174,6 +199,8 @@ WITH
     )
 
 
-SELECT *
-FROM join_td;
+    
+SELECT*
+FROM join_td
+
 
